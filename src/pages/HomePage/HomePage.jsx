808a30552666853fadd28b319260621ddda4ab2c
MaxKov86 <max.kov86@ukr.net>
2024-03-31 18:22:20 +0300

    Update files

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const HomePage = () => {
 	return (
 		<>
-			<h1>Phone Book</h1>
+			<h1>Your Phone Book</h1>
 			<p>
 				Hello! If you need a place to save your contacts and you don`t want to
 				lose them, then you`ve come to the right place!🙂 To get started, click
